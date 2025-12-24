f7bbaf416f495a6191ceb7c84257004088536d81
aswanthkn <aswanthkn.22cse@kongu.edu>
2025-12-24 11:34:30 +0530

    Added resend email

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: 'smtp.googlemail.com',
   port: 587,
   secure: false,
   auth: {
@@ -14,3 +14,32 @@ const transporter = nodemailer.createTransport({
 });
 
 export default transporter;
+
+// import { Resend } from 'resend';
+// import CustomError from './customError.js';
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// const transporter = {
+//   sendMail: async ({ to, subject, text, html }) => {
+//     try {
+//       const { data, error } = await resend.emails.send({
+//         from: process.env.EMAIL_FROM || 'SACL Consultancy <onboarding@resend.dev>',
+//         to: 'dhineshkumarans.22cse@kongu.edu',
+//         subject,
+//         text,
+//         html,
+//       });
+
+//       if (error) {
+//         throw new CustomError(error.message, error.statusCode || 500);
+//       }
+//       return data;
+//     } catch (err) {
+//       if (err instanceof CustomError) throw err;
+//       throw new CustomError(err.message, err.statusCode || 500);
+//     }
+//   }
+// };
+
+// export default transporter;
