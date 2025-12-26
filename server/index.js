68dd576504e7e210596f61718c9b16eff61f0665
aswanthkn <aswanthkn.22cse@kongu.edu>
2025-12-26 14:59:04 +0530

    Changes in CORS

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://sacl-consultancy-proj.vercel.app', 'https://sacl.onrender.com'],
+    origin: ['http://localhost:5173', 'https://sacl-consultancy.vercel.app', 'https://sacl-consultancy.onrender.com'],
     credentials: true
 }))
 
